65d9ca1419c429e7b3e0c72ba64c00868a841e04
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-02-22 12:46:53 +0100

    Define the controller to upload the file, using the already defined route

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 
 import views.html.*;
 
@@ -10,5 +12,30 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+
+	public static Result uploadSQL() {
+		MultipartFormData body = request().body().asMultipartFormData();
+
+		FilePart sqlFile = body.getFile("sqlFile");
+
+		if (sqlFile != null) {
+			String fileName = sqlFile.getFilename();
+			String contentType = sqlFile.getContentType();
+
+			if (FileValidator.hasExtension(fileName, "sql")) {
+				System.out.println("Processing SQL file...");
+
+				return ok(
+					"File " + fileName + " uploaded. Content-type: " +
+						contentType);
+			}
+
+			return badRequest("The file is not an SQL file");
+		}
+
+		flash("error", "Missing file");
+
+		return badRequest("The file has not been uploaded");
+	}
 
 }
