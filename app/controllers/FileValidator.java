0041c39f7ff0ca769dad401be92d4a361b7e6468
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-02-22 12:46:15 +0100

    Define a dummy extension validator

---
+++ app/controllers/FileValidator.java
@@ -0,0 +1,12 @@
+package controllers;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class FileValidator {
+
+	public static boolean hasExtension(String fileName, String extension) {
+		return true;
+	}
+
+}
