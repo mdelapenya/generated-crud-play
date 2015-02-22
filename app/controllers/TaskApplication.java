fe11c30e3f5f965b98aa55fe54bfac5a7b8dee54
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-02-22 19:07:43 +0100

    Add a method in the controller to display a task by id

--- app/controllers/TaskApplication.java
+++ app/controllers/TaskApplication.java
@@ -4,6 +4,7 @@ import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import views.html.task;
 import views.html.tasks;
 
 import java.util.List;
@@ -12,6 +13,12 @@ import java.util.List;
  * @author Manuel de la Peña
  */
 public class TaskApplication extends Controller {
+
+	public static Result get(Long id) {
+		Task dbTask = Task.find.byId(id);
+
+		return ok(task.render(dbTask));
+	}
 
 	public static Result all() {
 		List<Task> taskList = Task.find.all();
