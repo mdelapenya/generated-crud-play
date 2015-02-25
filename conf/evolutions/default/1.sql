8191d09c0066b2e9782607643f517769c1708a30
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-02-25 16:23:15 +0100

    Remove SQL creation of dummy model

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -9,18 +9,7 @@ create table foo (
   constraint pk_foo primary key (id))
 ;
 
-create table task (
-  id                        bigint not null,
-  name                      varchar(255),
-  street                    varchar(255),
-  done                      boolean,
-  due_date                  timestamp,
-  constraint pk_task primary key (id))
-;
-
 create sequence foo_seq;
-
-create sequence task_seq;
 
 
 
@@ -31,11 +20,7 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists foo;
 
-drop table if exists task;
-
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists foo_seq;
-
-drop sequence if exists task_seq;
 
