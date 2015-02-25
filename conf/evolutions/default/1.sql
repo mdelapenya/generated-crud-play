38dcdd158fa0ec86803764e50dbd417ff1ecbb8a
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-02-25 17:49:52 +0100

    Autogenerated

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,11 +3,19 @@
 
 # --- !Ups
 
+create table bar (
+  id                        bigint not null,
+  campo2                    varchar(255),
+  constraint pk_bar primary key (id))
+;
+
 create table foo (
   id                        bigint not null,
   f2                        varchar(255),
   constraint pk_foo primary key (id))
 ;
+
+create sequence bar_seq;
 
 create sequence foo_seq;
 
@@ -18,9 +26,13 @@ create sequence foo_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists bar;
+
 drop table if exists foo;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists bar_seq;
 
 drop sequence if exists foo_seq;
 
